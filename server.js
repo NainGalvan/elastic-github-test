0625ed03b995d8316af535113e42d88090d8e9e0
ngalvan <naingalvan12@gmail.com>
2022-06-22 03:37:52 -0700

    Fixing cannot get / error

--- server.js
+++ server.js
@@ -3,5 +3,7 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.get("/", (req,res) => res.send("Hello world"));
+
 app.listen(port, () => console.log(`listening on port ${port}`));
 
